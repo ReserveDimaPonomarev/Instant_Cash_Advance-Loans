080f384910ee0bc390dcae7574179345830b8eb4
Dima <nudelman1995@mail.ru>
2024-02-08 10:04:46 +0300

    added to appstore

--- LoanApp/AppDelegate.swift
+++ LoanApp/AppDelegate.swift
@@ -71,22 +71,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: SceneDelegate support - start AppsFlyer SDK
     
     @objc func didBecomeActiveNotification() {
-        AppsFlyerLib.shared().start()
-        if #available(iOS 14, *) {
-          ATTrackingManager.requestTrackingAuthorization { (status) in
-            switch status {
-            case .denied:
-                print("AuthorizationSatus is denied")
-            case .notDetermined:
-                print("AuthorizationSatus is notDetermined")
-            case .restricted:
-                print("AuthorizationSatus is restricted")
-            case .authorized:
-                print("AuthorizationSatus is authorized")
-            @unknown default:
-                fatalError("Invalid authorization status")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            AppsFlyerLib.shared().start()
+            if #available(iOS 14, *) {
+                
+                ATTrackingManager.requestTrackingAuthorization { (status) in
+                    switch status {
+                    case .denied:
+                        print("AuthorizationSatus is denied")
+                    case .notDetermined:
+                        print("AuthorizationSatus is notDetermined")
+                    case .restricted:
+                        print("AuthorizationSatus is restricted")
+                    case .authorized:
+                        print("AuthorizationSatus is authorized")
+                    @unknown default:
+                        fatalError("Invalid authorization status")
+                    }
+                }
             }
-          }
         }
     }
     
